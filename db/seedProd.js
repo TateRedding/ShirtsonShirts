c271f030b909011240decc3df92b6d411cbd38eb
Tate Redding <tateredding@gmail.com>
2023-08-31 11:59:31 -0600

    removed unnecessary files

--- db/seedProd.js
+++
@@ -1,8 +0,0 @@
-const client = require('./client');
-const { rebuildDB } = require('./seedData');
-const { seedDBProd } = require('./seedDataProd');
-
-rebuildDB()
-    .then(seedDBProd)
-    .catch(console.error)
-    .finally(() => client.end());
